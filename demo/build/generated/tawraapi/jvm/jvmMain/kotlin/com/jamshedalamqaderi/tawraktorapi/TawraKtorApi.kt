3b805b59babb18b637aa80726a3ee54e5e567f4c
Jamshed Alam Qaderi <jamshedalamqaderi@gmail.com>
2022-11-30 18:52:02 +0600

    undo

--- demo/build/generated/tawraapi/jvm/jvmMain/kotlin/com/jamshedalamqaderi/tawraktorapi/TawraKtorApi.kt
+++
@@ -1,9 +0,0 @@
-package com.jamshedalamqaderi.tawraktorapi
-
-import com.jamshedalamqaderi.tawraktorapi.rest.simpleApiExample
-import io.ktor.server.routing.Route
-import kotlin.Unit
-
-public fun Route.tawraKtorApiRoutes(): Unit {
-  simpleApiExample()
-}
--- demo/build/generated/tawraapi/metadata/commonMain/kotlin/com/jamshedalamqaderi/tawraktorapi/TawraKtorClientApi.kt
+++
@@ -1,4 +0,0 @@
-package com.jamshedalamqaderi.tawraktorapi
-
-val io.ktor.client.HttpClient.simpleApiExample
-    get() = com.jamshedalamqaderi.tawraktorapi.rest.SimpleApiExample.getInstance(this)
